2773c9789e82792605a2490d3a84d1431cf7f8be
Madhumithra916 <82104798+Madhumithra916@users.noreply.github.com>
2021-04-11 19:00:25 +0530

    positive_numbers.py
    the following code prints the positive numbers in list1 and list 2 separately

---
+++ positive_numbers.py
@@ -0,0 +1,16 @@
+List1=[12,-7,5,64,-14]
+print("The positive numbers in the list1 are:")
+for num in List1:
+    if num>0:
+       print(num)
+
+List2=[12,14,-95,3]
+print("The positive numbers in list2 are:")
+for num in List2:
+    if num>0:
+        print(num)
+
+
+       
+
+       
